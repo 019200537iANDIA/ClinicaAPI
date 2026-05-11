3b5e04ff85a5f07473acc7d9a10481bfbdfcf1a9
Jose Adelky Andia Sullcaccori <019200537i@uandina.edu.pe>
2026-05-11 15:15:08 -0500

    Primer commit ClinicaAPI

---
+++ ClinicaContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using ClinicaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ClinicaAPI
+{
+    public class ClinicaContextFactory : IDesignTimeDbContextFactory<ClinicaContext>
+    {
+        public ClinicaContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ClinicaContext>();
+            var connectionString =
+                "server=localhost;database=clinica_db;user=root;password=;";
+            optionsBuilder.UseMySql(connectionString,
+                ServerVersion.AutoDetect(connectionString));
+            return new ClinicaContext(optionsBuilder.Options);
+        }
+    }
+}
