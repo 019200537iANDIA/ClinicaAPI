47880511e8e6bc2d0e647e90aa00c2d5481d6eea
Jose Adelky Andia Sullcaccori <019200537i@uandina.edu.pe>
2026-05-11 15:19:44 -0500

    Implementacion CI con GitHub Actions

--- obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5e04ff85a5f07473acc7d9a10481bfbdfcf1a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
